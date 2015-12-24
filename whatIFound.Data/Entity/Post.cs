b64ad721e41cc61a17fec559fc1519a15589a469
Tiago Lopes <tiago92.lixa.2010@gmail.com>
2015-12-24 12:05:48 +0000

    Set entities
    set entities to generate database from nugets

---
+++ whatIFound.Data/Entity/Post.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace whatIFound.Data.Entity
+{
+    public class Post
+    {
+        public int PostId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime LastUpdateDate { get; set; }
+        public DateTime? DeleteDate { get; set; }
+    }
+}
